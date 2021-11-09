661ffac5c7feeae416da483c0b3a8279f983b2f7
EU\jekimkon <konstantins.jekimovs@tietoevry.com>
2021-11-09 12:23:18 +0200

    save by line

--- inchlv/helpers.py
+++ inchlv/helpers.py
@@ -2,7 +2,6 @@ class Helpers():
 
     def save_file(self, output="output.txt", encoding="utf-8"):
         with open(output, mode="w", encoding=encoding) as w:
-            #w.writelines(f"{str(self)}\n")
             w.write('\n'.join(str(line) for line in self))
             print(f"Result saved in {output}")
 
